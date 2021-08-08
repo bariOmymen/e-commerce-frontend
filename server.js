69933d78364f624654c26710b3e6ed62a71c1a58
bariOmymen <bariOmymen@gmail.com>
2021-08-08 13:39:02 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,"/build/index.html"))
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`current port is ${port}`)
